69086941da025d5da8268f4577807077f438f3e3
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-10-18 11:47:11 -0700

    Test other endpoints - test other model endpoints - set sleep time in .travis.yml - verify MD5 hashes of files downloaded from COS

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,34 @@ import pytest
 import requests
 
 
-def test_response():
+def test_swagger():
+
+    model_endpoint = 'http://localhost:5000/swagger.json'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+
+    json = r.json()
+    assert 'swagger' in json
+    assert json.get('info') and json.get('info').get('title') == 'Model Asset Exchange Server'
+
+
+def test_metadata():
+
+    model_endpoint = 'http://localhost:5000/model/metadata'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+
+    metadata = r.json()
+    assert metadata['id'] == 'resnet18_places365-pytorch-places365'
+    assert metadata['name'] == 'resnet18_places365 Pytorch Model'
+    assert metadata['description'] == 'resnet18_places365 Pytorch model trained on Places365'
+    assert metadata['license'] == 'CC BY'
+
+
+def test_predict():
     model_endpoint = 'http://localhost:5000/model/predict'
     file_path = 'assets/bakery.jpg'
 
